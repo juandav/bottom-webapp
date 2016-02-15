8cbf376b89c96d6bfceb629e015b3e9f10474dae
Juan David <e.juandav@gmail.com>
2016-02-15 13:16:04 -0500

    crud added to the menu

--- src/stores/MenuStore.jsx
+++ src/stores/MenuStore.jsx
@@ -11,11 +11,51 @@ let MenuStore = Reflux.createStore({
   mixins         : [ StateMixin.store ],
   listenables    : [ MenuActions ],
   url            : 'http://localhost:7000/menu',
-  getInitialState: function() {},
-  fetchMenu      : function() {},
-  createMenu     : function() {},
+  getInitialState: function() {
+    return {
+      menu: ''
+    };
+  },
+  fetchMenu      : function() {
+    $.ajax({
+      type   : 'GET',
+      url    : this.url,
+      context: this
+    })
+    .done(function(data) {
+      let info = JSON.stringify(data);
+      this.setState({ menu: info });
+    })
+    .fail(function(err) {
+      console.log('Error loading data: ' + err);
+    });
+  },
+  createMenu     : function(info) {
+    $.ajax({
+      type   : 'POST',
+      data   : info,
+      url    : this.url
+    })
+    .done(function(data) {
+      console.log(data);
+    })
+    .fail(function(err) {
+      console.log('Error loading data: ' + err);
+    });
+  },
   putMenu        : function() {},
-  removeMenu     : function() {}
+  removeMenu     : function() {
+    $.ajax({
+      url: 'http://localhost:7000/menu/' + data._id,
+      type: 'DELETE'
+    })
+    .done(function(data) {
+      console.log(data);
+    })
+    .fail(function(err) {
+      console.log('Error loading data: ' + err);
+    });
+  }
 });
 
 export default MenuStore;
