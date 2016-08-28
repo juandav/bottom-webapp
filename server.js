6b6bfae079856c67923589f1c89217e58fe7a982
Juan David <e.juandav@gmail.com>
2016-08-28 16:29:03 -0500

    fix
    Ahora webapp, continua entrando a las rutas sin perder acceso.

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var path = require('path');
 
 var app = express();
 
+app.set('views', `${__dirname}/public`)
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.render('index')
+})
 
 app.listen(8080, 'localhost', function(){
   console.log('server fun');
